602231a2e1806e85dd78093c1488a2ad43fe71cf
Shakib Hassan <hasanshakib74@gmail.com>
2019-06-09 23:37:34 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+           	
+    // -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+
+        var scroll = new SmoothScroll('a[href*="#"]');
+
+
+
+    });
+
+
+    jQuery(window).load(function(){
+        
+        // Protfolio JS
+        jQuery(".grid").isotope();
+
+    });
+
+
+}(jQuery));	
